63f2204e8b7bff3b5bee1efd10506de905300475
carlos <carlosdemetrio390@gmail.com>
2023-09-12 16:54:19 -0600

    segundo commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import "./assets/css/App.css"
+import NavBar from "./components/NavBar"
+import WeatherPanel from "./components/WeatherPanel"
+ 
+function App() {
+   
+  return (
+    <>
+      <div className="container2">
+        <NavBar/>
+        <WeatherPanel/>
+         
+      </div>
+      
+    </>
+  )
+}
+
+export default App
